124d8861ab0a07e88197c78f941933a3dc596ab7
LOGESWARAN001 <logeswaran0178@gmail.com>
2025-02-12 13:51:01 +0530

    Second Component Testing

---
+++ src/webparts/testWebPart001/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ITestWebPart001WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'TestWebPart001WebPartStrings' {
+  const strings: ITestWebPart001WebPartStrings;
+  export = strings;
+}
